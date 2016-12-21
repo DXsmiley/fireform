eeb7182249335951fed2d471a2a187044b1dbb82
DXsmiley <dxsmiley@hotmail.com>
2016-12-21 11:49:24 +1100

    Update the template so it's not broken

--- template/__main__.py
+++ template/__main__.py
@@ -1,22 +1,22 @@
 """
 
-Chaos
-
-A demonstration game for FireForm.
+A simple demonstration game for FireForm.
 
 """
 
 import fireform
+
+fireform.engine.load('pyglet')
 
 # Load the resources. See resources.json for more details.
 fireform.resource.load('./template/resources/')
 
 # Create the world
 world = fireform.world.world()
-world.add_system(fireform.system.debug())
 world.add_system(fireform.system.motion())
-world.add_system(fireform.system.image())
 world.add_system(fireform.system.camera())
+world.add_system(fireform.system.image())
+world.add_system(fireform.system.debug())
 
 # Behaviour to draw the player to the mouse
 
@@ -27,7 +27,7 @@ class mouse_attraction(fireform.behaviour.base):
 		self.mouse_y = 0
 
 	def m_tick(self, world, entity, message):
-		pos = entity['position']
+		pos = entity['box'].position
 		acc = entity['acceleration']
 		acc.x = (self.mouse_x - pos.x) / 200
 		acc.y = (self.mouse_y - pos.y) / 200
@@ -36,24 +36,20 @@ class mouse_attraction(fireform.behaviour.base):
 		self.mouse_x = message.x
 		self.mouse_y = message.y
 
-	def name(self):
-		return 'mouse_attraction'
-
 # Spawn the objects
 
 def make_player():
 	return fireform.entity.entity(
-		fireform.data.position(100, 100),
+		fireform.data.box(x = 100, y = 100, width = 64, height = 64),
 		fireform.data.velocity(0, 0),
 		fireform.data.acceleration(0, 0),
 		fireform.data.image('face'),
-		fireform.data.size(64, 64),
 		mouse_attraction()
 	)
 
 def make_focus():
 	return fireform.entity.entity(
-		fireform.data.position(0, 0),
+		fireform.data.box(0, 0),
 		fireform.data.camera()
 	)
 
