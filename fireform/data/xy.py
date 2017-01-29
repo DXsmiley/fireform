2d9992b9a85404699d2c66d21da021c86b112a97
DXsmiley <dxsmiley@hotmail.com>
2017-01-29 16:19:41 +1100

    Add .vector property to xy datum to allow easy manipulation and assignment

--- fireform/data/xy.py
+++ fireform/data/xy.py
@@ -3,3 +3,12 @@ from fireform.geom import vector
 
 class xy(vector, base):
 	"""Base class for vector-based datum to inherit from."""
+
+	@property
+	def vector(self):
+		return vector(self.x, self.y)
+
+	@vector.setter
+	def vector(self, v):
+		self.x = v.x
+		self.y = v.y
