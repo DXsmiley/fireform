85b87d7a97893657b37e025ab9193164a3e51374
DXsmiley <dxsmiley@hotmail.com>
2017-01-30 11:20:24 +1100

    Improve friction component

--- fireform/data/friction.py
+++ fireform/data/friction.py
@@ -1,12 +1,16 @@
-from fireform.data.base import base
+from fireform.data.xy import xy
 from fireform.geom import vector
 
-class friction(vector, base):
+class friction(xy):
 	"""Multiplies the velocity of an entity every tick, causing it to accelerate or decelerate."""
 
 	name = 'friction'
 	attribute_name = 'friction'
 
-	def __init__(self, x = 1, y = 1):
-		self.x = x
-		self.y = y
+	def __init__(self, *args):
+		if len(args) == 0:
+			self.x = self.y = 0
+		elif len(args) == 1:
+			self.x = self.y = args[0]
+		else:
+			self.x, self.y = args
